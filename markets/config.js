efe99fa1944ade72cedf442b7c52efe5c0124c0d
Eyouel Melaku <EyouelM@Es-MacBook-Pro-Max.local>
2026-07-17 19:36:39 -0400

    Align markets review scopes

--- markets/config.js
+++ markets/config.js
@@ -2,5 +2,5 @@ window.EYOUEL_MARKETS_CONFIG = {
   appName: 'Eyouel Markets',
   clientKey: 'REPLACE_WITH_MARKETS_CLIENT_KEY',
   redirectUri: 'https://eyouel-cloud.github.io/markets/auth/tiktok/callback.html',
-  scopes: 'user.info.basic,video.publish'
+  scopes: 'user.info.basic,video.publish,video.upload'
 };
